0e785950fd0ae31be5a67c3fd82a5708d50fe532
rafy <rafy683@gmail.com>
2022-03-09 09:11:59 +0700

    update

--- app/Orchid/Screens/PlatformScreen.php
+++ app/Orchid/Screens/PlatformScreen.php
@@ -15,14 +15,14 @@ class PlatformScreen extends Screen
      *
      * @var string
      */
-    public $name = 'Get Started';
+    public $name = 'Dashboard';
 
     /**
      * Display header description.
      *
      * @var string
      */
-    public $description = 'Welcome to your Orchid application.';
+    public $description = 'Welcome to your application.';
 
     /**
      * Query data.
@@ -41,19 +41,7 @@ class PlatformScreen extends Screen
      */
     public function commandBar(): array
     {
-        return [
-            Link::make('Website')
-                ->href('http://orchid.software')
-                ->icon('globe-alt'),
-
-            Link::make('Documentation')
-                ->href('https://orchid.software/en/docs')
-                ->icon('docs'),
-
-            Link::make('GitHub')
-                ->href('https://github.com/orchidsoftware/platform')
-                ->icon('social-github'),
-        ];
+        return [];
     }
 
     /**
@@ -64,7 +52,7 @@ class PlatformScreen extends Screen
     public function layout(): array
     {
         return [
-            Layout::view('platform::partials.welcome'),
+            //Layout::view('platform::partials.welcome'),
         ];
     }
 }
