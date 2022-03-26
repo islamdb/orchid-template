cb34509251ae8f44c02cfe82b6f82184b113c5ef
rafy <rafy683@gmail.com>
2022-03-26 11:28:29 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        require_once(__DIR__ . '/../Support/helpers.php');
+
+        Collection::macro('onlyAttr', function ($keys) {
+            $keys = is_array($keys)
+                ? $keys
+                : [$keys];
+
+            return $this->map(function ($value) use ($keys) {
+                return collect($value)
+                    ->only($keys)
+                    ->toArray();
+            });
+        });
+
+        Blueprint::macro('money', function ($column) {
+            return $this->decimal($column, 40, 25);
+        });
+
+        // Schema::defaultStringLength(191);
     }
 }
